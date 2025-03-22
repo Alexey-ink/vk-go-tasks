d7170510d6e781f642c2921a116ac94798f98067
Alexey Shihalev <alesha.shikhalev@mail.ru>
2025-03-22 05:26:35 +0300

    refactor: remove unused method

--- text_game/player.go
+++ text_game/player.go
@@ -19,15 +19,3 @@ func (p Player) String() string {
 	}
 	return fmt.Sprintf("Name of player: %s \nCurrent room: %v \nInventory: [%v]\n", p.Name, p.CurrentRoom, ItemStr)
 }
-
-func (p Player) Take(item string) string {
-	if p.BackpackOn {
-		*p.Inventory = append(*p.Inventory, item)
-		return fmt.Sprintf(("предмет добавлен в инвентарь: %s"), item)
-	} else if item == "рюкзак" {
-		p.BackpackOn = true
-		return "вы надели: рюкзак"
-	} else {
-		return "некуда класть"
-	}
-}
